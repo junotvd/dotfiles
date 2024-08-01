7f176953716b92f3eedb9f01ca232a8ad313672f
Fedora <vdjunot@gmail.com>
2024-08-01 15:11:44 +0200

    removed material gruvbox

--- .config/nvim/lua/plugins/gruvbox-material.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"sainnhe/gruvbox-material",
-	priority = 1000, -- Make sure to load this before all the other start plugins.
-	lazy = false,
-	config = function()
-		vim.g.gruvbox_material_enable_italic = true
-		vim.cmd.colorscheme("gruvbox-material")
-	end,
-}
