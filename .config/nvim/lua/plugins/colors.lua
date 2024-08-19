b0062626dfe7b4aa6defbc67322d17245c82a268
Fedora <vdjunot@gmail.com>
2024-08-19 13:04:27 +0200

    different file for colors

---
+++ .config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,18 @@
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	config = function()
+-- 		require("gruvbox").setup({})
+-- 		vim.cmd("colorscheme gruvbox")
+-- 	end
+-- }
+
+return {
+	'rose-pine/neovim',
+	name = 'rose-pine',
+	config = function()
+		-- disable_background = true
+		vim.cmd('colorscheme rose-pine')
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	end,
+}
