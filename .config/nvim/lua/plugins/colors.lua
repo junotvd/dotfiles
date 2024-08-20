65aebc9ad02142498ce54db52308ccb58a0a1002
Fedora <vdjunot@gmail.com>
2024-08-20 15:57:41 +0200

    finalized

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,12 +1,41 @@
+-- Copy of ThePrimeagen
+
+function ColorMyPencils(color)
+	color = color or 'rose-pine'
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
-	'rose-pine/neovim',
-	name = 'rose-pine',
-	config = function()
-		require("rose-pine").setup({
-			variant = "main",
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" }),
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }),
-			vim.cmd('colorscheme rose-pine')
-		})
-	end,
+	{
+		'rose-pine/neovim',
+		name = 'rose-pine',
+		config = function()
+			require("rose-pine").setup({
+				variant = "main",
+				disable_background = true,
+			})
+
+			ColorMyPencils()
+		end,
+	},
+
+	{
+		'folke/tokyonight.nvim',
+		config = function()
+			require('tokyonight').setup({
+				style = 'storm',
+				transparent = true,
+				terminal_colors = true,
+				styles = {
+					comments = { italic = false},
+					keywords = { italic = false},
+					sidebars = 'dark',
+					floats = 'dark',
+				},
+			})
+		end,
+	},
 }
