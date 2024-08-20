83c8e7e0381a33704a8830f8a6b4d72a10d7085b
Fedora <vdjunot@gmail.com>
2024-08-20 14:15:35 +0200

    changed to rose-pine

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,28 +1,16 @@
--- return {
--- 	"ellisonleao/gruvbox.nvim",
--- 	config = function()
--- 		require("gruvbox").setup({})
--- 		vim.cmd("colorscheme gruvbox")
--- 	end
--- }
-
--- return {
--- 	'rose-pine/neovim',
--- 	name = 'rose-pine',
--- 	config = function()
--- 		-- disable_background = true
--- 		vim.cmd('colorscheme rose-pine')
--- 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- 	end,
--- }
-
 return {
-	'rebelot/kanagawa.nvim',
+	'rose-pine/neovim',
+	name = 'rose-pine',
 	config = function()
-		require('kanagawa').setup({
-			theme = 'dragon',
+		require("rose-pine").setup({
+			variant = "main",
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" }),
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }),
+			styles = {
+				bold = false,
+				italic = true,
+			},
+			vim.cmd('colorscheme rose-pine')
 		})
-		vim.cmd('colorscheme kanagawa-dragon')
-	end
+	end,
 }
