5c07b0851fda27625afe1f4ffe22b062b3b2e361
Fedora <vdjunot@gmail.com>
2024-11-21 11:00:08 +0100

    reorg

--- .config/nvim/ftplugin/markdown.lua
+++
@@ -1,6 +0,0 @@
--- Spell checking
-vim.opt_local.spell = true
-vim.opt.spelllang = { "nl", "en_gb" }
-
--- Colorscheme
-vim.cmd.colorscheme{"nord"}
--- .config/nvim/lua/plugins/LuaSnip/settings.lua
+++
@@ -1,9 +0,0 @@
-return {
-    -- Jump to the next placeholder
-    vim.api.nvim_set_keymap("i", "<Tab>", "<cmd>lua require'luasnip'.jump(1)<CR>", {silent = true}),
-    vim.api.nvim_set_keymap("s", "<Tab>", "<cmd>lua require'luasnip'.jump(1)<CR>", {silent = true}),
-
-    -- Jump to the previous placeholder
-    vim.api.nvim_set_keymap("i", "<S-Tab>", "<cmd>lua require'luasnip'.jump(-1)<CR>", {silent = true}),
-    vim.api.nvim_set_keymap("s", "<S-Tab>", "<cmd>lua require'luasnip'.jump(-1)<CR>", {silent = true}),
-}
