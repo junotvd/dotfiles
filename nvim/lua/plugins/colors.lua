c64c9eb3d8cbd89251f302c9ea9e49044be16561
junot <vdjunot@gmail.com>
2024-12-06 02:26:45 +0100

    testing themes

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,41 +1,25 @@
 return {
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("rose-pine").setup({
-                variant = "main",
-                disable_background = true,
-                disable_float_background = true,
-                styles = {
-                    italic = true,
-                    transparent = false,
-                },
-                highlight_groups = {
-                    TreesitterContext = { bg = "NONE" },
-                    TreesitterContextLineNumber = { bg = "NONE" },
-
-                }
-            })
-            vim.api.nvim_set_hl(0, "Float", { bg = "none" })
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-            vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-            -- vim.cmd.colorscheme("rose-pine")
-        end
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            -- vim.cmd.colorscheme("kanagawa-dragon")
-        end
-    },
-    {
-        "ricardoraposo/gruvbox-minor.nvim",
-        config = function()
-            vim.cmd.colorscheme("gruvbox-minor")
-        end
-    },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      -- vim.cmd.colorscheme("kanagawa-dragon")
+    end
+  },
+  {
+    "ricardoraposo/gruvbox-minor.nvim",
+    config = function()
+      -- vim.cmd.colorscheme("gruvbox-minor")
+    end
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("solarized-osaka").setup({
+        transparent = false
+      })
+      vim.cmd.colorscheme("solarized-osaka")
+    end
+  },
 }
