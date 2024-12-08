47a7128b723e1c6917641aa8f478110888f78591
junot <vdjunot@gmail.com>
2024-12-08 15:42:11 +0100

    nvim changes

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -2,7 +2,10 @@ return {
   {
     "rebelot/kanagawa.nvim",
     config = function()
-      -- vim.cmd.colorscheme("kanagawa-dragon")
+      require("kanagawa").setup({
+        transparent = false
+      })
+      vim.cmd.colorscheme("kanagawa-dragon")
     end
   },
   {
@@ -14,12 +17,19 @@ return {
   {
     "craftzdog/solarized-osaka.nvim",
     lazy = false,
-    priority = 1000,
+    -- priority = 1000,
     config = function()
       require("solarized-osaka").setup({
         transparent = false
       })
-      vim.cmd.colorscheme("solarized-osaka")
+      -- vim.cmd.colorscheme("solarized-osaka")
     end
   },
+  -- {
+  --   "ppoxx",
+  --   dir = "~/colors/",
+  --   config = function()
+  --     vim.cmd.colorscheme("ppoxx")
+  --   end
+  -- }
 }
--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,6 +1,9 @@
 return {
-    'MeanderingProgrammer/markdown.nvim',
-    main = "render-markdown",
-    opts = {},
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  'MeanderingProgrammer/markdown.nvim',
+  main = "render-markdown",
+  opts = {},
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'echasnovski/mini.nvim'
+  },
 }
