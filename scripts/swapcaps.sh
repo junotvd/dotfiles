7d531e3d0a9f75e9b219441e7d533adc36b2cd59
junot <vdjunot@gmail.com>
2024-12-19 17:04:03 +0100

    added exit option

--- scripts/swapcaps.sh
+++ scripts/swapcaps.sh
@@ -2,12 +2,15 @@
 
 # this makes you choose wheter or not to swap ctrl and capslock key
 
-choice=$(echo "swap\nregular" |dmenu -i -p "ctrl and caplocks keys:")
+choice=$(echo "swap\nregular\nexit" |dmenu -i -p "ctrl and caplocks keys:")
 case "$choice" in
     "swap")
         setxkbmap -layout us -option ctrl:swapcaps
         ;;
     "regular")
         setxkbmap -layout us -option
+        ;;
+    "exit")
+        exit
         ;;
 esac
