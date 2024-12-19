ae6a908e6e4c478c71fbbda70ae80dbe057682c4
junot <vdjunot@gmail.com>
2024-12-19 15:04:08 +0100

    if only connected display is built-in laptop display -> hardcode settings are used

--- scripts/select-display.sh
+++ scripts/select-display.sh
@@ -3,6 +3,10 @@
 # This script is made for single monitor use
 
 monoscreen() {
+    [ "$1" = "eDP" ] &&
+        rate=$(echo "60\n120" | dmenu -i -p "refresh rate:")
+        { xrandr --output eDP --mode 2880x1920 --rate "$rate" --scale 1x1; exit; }
+
     res=$(xrandr --query | sed -n "/^$1/,/\+/p" | tail -n 1 | awk '{print $1}')
     xrandr --output "$1" --auto --scale 1x1
 }
@@ -14,7 +18,7 @@ mirrorscreen() {
 screens=$(xrandr -q | grep "connected" | awk '/ connected/ {print $1}')
 
 [ "$(echo "$screens" | wc -l)" -lt 2 ] &&
-    { monoscreen "$screens"; notify-send "only one available screen"; exit; }
+{ monoscreen "$screens"; dmenu -p "only one available screen"; exit; }
 
 selected=$(echo "$screens" | dmenu -i -p "select screen")
 other=$(echo "$screens" | grep -v "^$selected")
