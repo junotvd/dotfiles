ff98e56191db5b9de899789afc86991be7ba4af8
junot <vdjunot@gmail.com>
2024-12-19 16:57:46 +0100

    swapcaps

---
+++ scripts/swapcaps.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# this makes you choose wheter or not to swap ctrl and capslock key
+
+choice=$(echo "swap\nregular" |dmenu -i -p "ctrl and caplocks keys:")
+case "$choice" in
+    "swap")
+        setxkbmap -layout us -option ctrl:swapcaps
+        ;;
+    "regular")
+        setxkbmap -layout us -option
+        ;;
+esac
