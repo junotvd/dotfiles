c3f50cb67979d012f138e53a1e7958b7a1f2f833
junot <vdjunot@gmail.com>
2025-01-16 18:17:31 +0100

    added format keymap but the default is also not bad so probably not keeping it

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -12,3 +12,6 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
+vim.keymap.set("n", "<leader>x", ":lua<CR>")
+vim.keymap.set("v", "<leader>x", ":lua<CR>")
