2935d394cc21b8db80d4b27ec76decd8dd44c1af
junot <vdjunot@gmail.com>
2025-02-01 21:33:59 +0100

    taalsnippets

---
+++ nvim/lua/snippets/all/dutch.lua
@@ -0,0 +1,26 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+
+return {
+  s({ trig = "asa" }, { t("als en slechts als") }),
+  s({ trig = "asa" }, { t("als en slechts als") }),
+  s({ trig = "dwz" }, { t("dit wil zeggen") }),
+  s({ trig = "Dwz" }, { t("Dit wil zeggen") }),
+  s({ trig = "dmv" }, { t("door middel van") }),
+  s({ trig = "Dmv" }, { t("Door middel van") }),
+  s({ trig = "ifv" }, { t("in functie van") }),
+  s({ trig = "Ifv" }, { t("In functie van") }),
+  s({ trig = "ipv" }, { t("in plaats van") }),
+  s({ trig = "Ipv" }, { t("In plaats van") }),
+  s({ trig = "maw" }, { t("met andere woorden") }),
+  s({ trig = "Maw" }, { t("Met andere woorden") }),
+  s({ trig = "mbt" }, { t("met betrekking tot") }),
+  s({ trig = "Mbt" }, { t("Met betrekking tot") }),
+  s({ trig = "nl" }, { t("namelijk") }),
+  s({ trig = "Nl" }, { t("Namelijk") }),
+  s({ trig = "resp" }, { t("respectievelijk") }),
+  s({ trig = "Resp" }, { t("Respectievelijk") }),
+  s({ trig = "tov" }, { t("ten opzichte van") }),
+  s({ trig = "Tov" }, { t("Ten opzichte van") }),
+}
