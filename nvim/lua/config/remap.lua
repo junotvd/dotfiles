b41f00f803243d454eed724d5367bd517688549b
junot <vdjunot@gmail.com>
2025-02-01 12:47:51 +0100

    highligh on yank

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -13,5 +13,14 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
-vim.keymap.set("n", "<leader>x", ":lua<CR>")
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<leader>x", ":.lua<CR>")
 vim.keymap.set("v", "<leader>x", ":lua<CR>")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true}),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
