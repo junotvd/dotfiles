4426202ce7c3d0ee90d350fb5e18a766b8290586
junot <vdjunot@gmail.com>
2025-02-01 16:23:35 +0100

    quickfix remap

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -17,6 +17,9 @@ vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<leader>x", ":.lua<CR>")
 vim.keymap.set("v", "<leader>x", ":lua<CR>")
 
+vim.keymap.set("n", "<leader>qn", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<leader>qp", "<cmd>cprev<CR>")
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true}),
