85bf3263fdc35ea030294aa794898e50dfcb314c
junot <vdjunot@gmail.com>
2025-02-01 16:23:57 +0100

    fzf native dependency

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,9 @@
 return {
   'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
