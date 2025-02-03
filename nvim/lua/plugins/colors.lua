5826ef06bf5d6a09d6674ac14d4793bdc990b67f
junot <vdjunot@gmail.com>
2025-02-03 20:20:06 +0100

    voor de verandering

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -5,9 +5,19 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("nord")
+      vim.cmd.colorscheme("rose-pine")
+      vim.cmd("highlight Normal guibg=black")
+      vim.cmd("highlight NonText guibg=black")
     end,
   },
+  "rose-pine/neovim",
+  {
+    -- "projekt0n/github-nvim-theme",
+    -- config = function()
+    --   vim.cmd.colorscheme("github_dark")
+    -- end
+  },
+
   -- Colorizer
   {
     "norcalli/nvim-colorizer.lua",
