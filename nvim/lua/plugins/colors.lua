bdaf25e3f4db71d74e053f2fd9ac47198a8b98da
junot <vdjunot@gmail.com>
2025-02-05 08:24:45 +0100

    toch niet nuttig

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,59 +1,41 @@
-local nord = {
-  "shaunsingh/nord.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.g.nord_bold = false
-    vim.cmd.colorscheme("nord")
-  end,
-}
+function NoBackground()
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
 
-local rose_pine = {
-  "rose-pine/neovim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  end,
-}
-
-local slugbyte = {
-  "slugbyte/lackluster.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("slugbyte")
-  end
-}
-local no_clown_fiesta = {
-  "aktersnurra/no-clown-fiesta.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("slugbyte")
-  end
-}
-
-local solarized = {
-  "maxmx03/solarized.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- vim.o.background = 'light'
-    require("solarized").setup({
-      variant = "spring",
-    })
-    vim.cmd.colorscheme("solarized")
-  end,
-}
+return {
+  {
+    "shaunsingh/nord.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.nord_bold = false
+      vim.cmd.colorscheme("nord")
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    config = function()
+      -- NoBackground()
+    end,
+  },
+  {
+    "maxmx03/solarized.nvim",
+    config = function()
+      -- vim.o.background = 'light'
+      require("solarized").setup({
+        variant = "spring",
+      })
+    end,
+  },
+  { "slugbyte/lackluster.nvim" },
+  { "aktersnurra/no-clown-fiesta.nvim" },
 
 
-local colorizer = {
-  "norcalli/nvim-colorizer.lua",
-  opts = {
-    "css"
-  }
+  {
+    "norcalli/nvim-colorizer.lua",
+    opts = {
+      "css"
+    }
+  },
 }
-
-return {nord, colorizer}
