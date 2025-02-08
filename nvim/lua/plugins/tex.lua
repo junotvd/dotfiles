5cf34efcb3a3f60f48aa78391de85e893e23c43b
junot <vdjunot@gmail.com>
2025-02-08 14:05:15 +0100

    inkscape integratie aan het uitproberen

--- nvim/lua/plugins/tex.lua
+++ nvim/lua/plugins/tex.lua
@@ -6,6 +6,10 @@ return {
       vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex -shell-escape" }
       vim.g.vimtex_view_method = "zathura"
       vim.api.nvim_set_hl(0, "Conceal", { ctermbg = "none" })
+      vim.cmd([[
+        inoremap <C-f> <Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>
+        nnoremap <C-f> : silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>
+        ]])
     end,
   },
   {
