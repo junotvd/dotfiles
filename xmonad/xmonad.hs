a172d0ec0a603e44bfd42fec2d81813a36c8c1f5
junot <vdjunot@gmail.com>
2025-02-08 14:06:03 +0100

    geen idee

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,13 +1,46 @@
 import XMonad
+import XMonad.Hooks.EwhmDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.BinarySpacePartition
 import XMonad.Util.EZConfig
 
-
 main :: IO ()
-main = xmonad $ def
+main = xmonad $ ewmh $ docks myConfig
+
+myConfig = def
     { modMask = mod1Mask
-    }
-  `additionalKeysP`
-    [ ("M-S-z", spawn "xscreensaver-command -lock"),
-      ("M-C-s", unGrab *> spawn "scrot -s"),
-      ("M-b", spawn "firefox")
-    ]
+    , layoutHook = myLayout
+    , manageHook = myManageHook
+    , startupHook =
+    , myStartupHook
+}
+`additionalKeysP` myKeys
+
+
+myTerminal :: String
+myTerminal = "wezterm"
+
+myBrowser :: String
+myBrowser = "firefox"
+
+myKeys :: [(Char, X ())]
+myKeys =
+[ ("M-t", spawn myTerminal),
+  ("M-b", spawn myBrowser)
+]
+
+myManageHook :: ManageHook
+myManageHook =
+composeAll
+[ className =? "Gimp" --> doFloat,
+    isDialog --> doFloat
+]
+
+myLayout = tiled ||| Mirror tiled ||| full ||| threeCol
+    where
+    tiled = Tall 1 (3 / 100) (1 / 2)
+threeCol = ThreeColMid 1 (3 / 100) (1 / 2)
+
+    myStartupHook = do
+    spawm "nm-applet"
