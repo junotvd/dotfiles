163f8fa0d053f0b87da4ed98104eb49b7289493a
junot <vdjunot@gmail.com>
2025-02-11 17:08:58 +0100

    waste of time

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -7,40 +7,19 @@ return {
   {
     "shaunsingh/nord.nvim",
     lazy = false,
-    priority = 1000,
     config = function()
       vim.g.nord_bold = false
-      vim.cmd.colorscheme("monokai-pro-classic")
+      vim.cmd.colorscheme("rose-pine")
+      NoBackground()
     end,
   },
-  {
-    "rose-pine/neovim",
-    config = function()
-      -- NoBackground()
-    end,
-  },
-  {
-    "maxmx03/solarized.nvim",
-    config = function()
-      -- vim.o.background = 'light'
-      require("solarized").setup({
-        variant = "spring",
-      })
-    end,
-  },
-  { "rebelot/kanagawa.nvim" },
-  { "slugbyte/lackluster.nvim" },
-  { "aktersnurra/no-clown-fiesta.nvim" },
-  { "ellisonleao/gruvbox.nvim" },
-  { "loctvl842/monokai-pro.nvim" },
-  { "rmehri01/onenord.nvim" },
-  { "nanotech/jellybeans.vim" },
-
-
-  {
-    "norcalli/nvim-colorizer.lua",
-    opts = {
-      "css"
-    }
-  },
+  "rose-pine/neovim",
+  "maxmx03/solarized.nvim",
+  "rebelot/kanagawa.nvim",
+  "slugbyte/lackluster.nvim",
+  "aktersnurra/no-clown-fiesta.nvim",
+  "ellisonleao/gruvbox.nvim",
+  "loctvl842/monokai-pro.nvim",
+  "rmehri01/onenord.nvim",
+  "nanotech/jellybeans.vim",
 }
