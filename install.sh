7e88ba0b5c6422f81591b51a3b498a675af2d582
junot <vdjunot@gmail.com>
2025-02-15 14:11:44 +0100

    recursief

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 ln -s ~/dotfiles/alacritty/ ~/.config/
 ln -s ~/dotfiles/i3/ ~/.config/
 ln -s ~/dotfiles/i3status/ ~/.config/
-cp ~/.vim ~/.vim_old; mkdir ~/.vim/; ln -s ~/dotfiles/vim/ftplugin/ ~/.vim/; ln -s ~/dotfiles/vim/spell/ ~/.vim/; ln -s ~/dotfiles/vim/UltiSnips/ ~/.config/; ln -s ~/dotfiles/nvim/ ~/.config/
+cp -r ~/.vim ~/.vim_old; mkdir ~/.vim/; ln -s ~/dotfiles/vim/ftplugin/ ~/.vim/; ln -s ~/dotfiles/vim/spell/ ~/.vim/; ln -s ~/dotfiles/vim/UltiSnips/ ~/.config/; ln -s ~/dotfiles/nvim/ ~/.config/
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/zathura/ ~/.config/
 
 ln -s ~/dotfiles/bash/.bashrc ~/
 ln -s ~/dotfiles/tmux/.tmux.conf ~/
 
+mkdir ~/.local/bin/
 ln -s ~/dotfiles/scripts/ ~/.local/bin/
