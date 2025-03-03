05e88e473fcf47f35350f222cc2efc01bbfef1d3
junot <vdjunot@gmail.com>
2025-03-03 10:33:46 +0100

    best handig zo

--- scripts/yazi.sh
+++ scripts/yazi.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-DIRS=$(find ~/ ~/uni/ ~/uni/bachelor-1/ ~/uni/bachelor-1/semester-1/ ~/uni/bachelor-1/semester-2/ ~/coding/ ~/Documents/ ~/repos ~/suckless/ ~/uni/bachelor-1/semester-1/calculus-1/lesmateriaal/ -mindepth 1 -maxdepth 1 -type d)
+DIRS=(
+    ~/
+    ~/uni/
+    ~/uni/bachelor-1/
+    ~/uni/bachelor-1/semester-1/
+    ~/uni/bachelor-1/semester-2/
+    ~/coding/
+    ~/Documents/
+    ~/repos
+    ~/suckless/
+    ~/uni/bachelor-1/semester-1/calculus-1/lesmateriaal/
+)
 
-CHOSEN_DIR=$(echo "$DIRS" | rofi -dmenu -p "Find directory")
+SEARCH_DIRS=$(find "${DIRS[@]}" -mindepth 1 -maxdepth 1 -type d 2>/dev/null | sed "s|$HOME|~|")
+CHOSEN_DIR=$(echo "$SEARCH_DIRS" | rofi -dmenu -p "Find directory")
+
+if [ -z "$CHOSEN_DIR" ]; then
+    exit 1
+fi
 
 # $TERMINAL -e "cd $CHOSEN_DIR && yazi"
 alacritty -e bash -c "yazi '$CHOSEN_DIR'; exec bash"
