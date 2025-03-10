7f26419001a784c699ea5638d88449b568c3e544
junot <vdjunot@gmail.com>
2025-03-10 10:29:33 +0100

    combined display and key scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+OPTION=$(echo -e "monitor\nkeys" | rofi -dmenu -i -p "hoorcolleges of oefenzittingen" -theme $HOME/dotfiles/rofi/black-white-selector.rasi)
+
+case "$OPTION" in
+    "keys")
+        exec $HOME/dotfiles/scripts/swapcaps.sh
+        ;;
+    "monitor")
+        exec $HOME/dotfiles/scripts/select-display.sh
+        ;;
+esac
