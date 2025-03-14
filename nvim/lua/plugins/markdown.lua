b4afe2012ae8448b1edd0cbaf7931b07c2bdd083
junot <vdjunot@gmail.com>
2025-03-14 14:33:40 +0100

    disabled table rendering

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,12 +1,21 @@
 return {
+
   {
     "MeanderingProgrammer/render-markdown.nvim",
+    config = function()
+      require("render-markdown").setup({
+        pipe_table = {
+          enabled = false,
+        },
+      })
+    end
   },
+
   {
     "iamcco/markdown-preview.nvim",
-    -- cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    -- ft = { "markdown" },
-    -- build = function() vim.fn["mkdp#util#install"]() end,
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
     config = function()
       vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>")
     end
