f35c876b41499cf0990c56243adbf6d901ab6e08
junot <vdjunot@gmail.com>
2025-03-14 14:02:05 +0100

    improv

--- scripts/select-display.sh
+++ scripts/select-display.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# This script is made for single monitor use
-
 monoscreen() {
+    local display="$1"
 
-    if [ "$1" = "eDP" ]; then
-        rate=$(printf "60\n120" | rofi -dmenu -i -p "refresh rate:" \
-            -theme ~/dotfiles/rofi/black-white-selector.rasi)
-        { xrandr --output eDP --mode 2880x1920 --rate "$rate" --scale 1x1; exit; }
+    available_rates=$(xrandr --verbose | grep -A 1 "^$display" | awk '/[0-9]+\.[0-9]+/ {print $1}' | sort -u)
 
-        if [ -n "$rate" ]; then
-            xrandr --output eDP --mode 2880x1920 --rate "$rate" --scale 1x1
-            exit 1
-        fi
+    if [ -z "$available_rates" ]; then
+        available_rates="60\n120"
     fi
 
-    xrandr --output "$1" --auto --scale 1x1
+    rate=$(echo -e "$available_rates" | rofi -dmenu -i -p "Select refresh rate for $display:" -theme ~/dotfiles/rofi/black-white-selector.rasi)
+
+    [ -z "$rate" ] && exit 1
+
+    resolution=$(xrandr | grep -A1 "^$display" | awk '/\*/ {print $1}')
+
+    xrandr --output "$display" --mode "$resolution" --rate "$rate" --scale 1x1
+    exit 0
 }
 
 mirrorscreen() {
@@ -23,17 +24,34 @@ mirrorscreen() {
     xrandr --output "$2" --auto --same-as "$1" --scale 0.75x0.75
 }
 
-screens=$(xrandr -q | grep "connected" | awk '/ connected/ {print $1}')
+screens=$(xrandr --listmonitors | awk 'NR>1 {print $4}')
 
-[ "$(printf "$screens" | wc -l)" -lt 2 ] &&
-{ monoscreen "$screens"; rofi -e "only one available screen"; exit; }
+screen_count=$(echo "$screens" | wc -l)
 
-selected=$(printf "$screens" | rofi -dmenu -i -p "select screen" -theme ~/dotfiles/rofi/black-white-selector.rasi)
-other=$(printf "$screens" | grep -v "^$selected")
-mirror=$(printf "no\nyes" | rofi -dmenu -i -p "want to mirror $selected onto $other?" -theme ~/dotfiles/rofi/black-white-selector.rasi)
+if [ "$screen_count" -lt 2 ]; then
+    primary_screen=$(echo "$screens" | head -n1)
+    monoscreen "$primary_screen"
+    rofi -e "Only one available screen"
+    exit
+fi
 
-[ "$mirror" = "yes" ] &&
-    { mirrorscreen $selected $other; exit; }
+selected=$(echo "$screens" | rofi -dmenu -i -p "Select screen:" -theme ~/dotfiles/rofi/black-white-selector.rasi)
 
+[ -z "$selected" ] && exit 1
 
-xrandr --output  "$selected" --auto && xrandr --output "$other" --off
+other=$(echo "$screens" | grep -v "^$selected")
+
+if [ -z "$other" ]; then
+    rofi -e "No other monitor detected"
+    exit 1
+fi
+
+mirror=$(printf "no\nyes" | rofi -dmenu -i -p "Mirror $selected onto $other?" -theme ~/dotfiles/rofi/black-white-selector.rasi)
+
+if [ "$mirror" = "yes" ]; then
+    mirrorscreen "$selected" "$other"
+    exit
+fi
+
+xrandr --output "$selected" --auto
+xrandr --output "$other" --off
