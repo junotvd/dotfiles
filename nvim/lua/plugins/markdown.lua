6e07e3b98379791ec884a68ef6862119e07e2b3f
junot <vdjunot@gmail.com>
2025-03-16 08:51:20 +0100

    markdown-preview don't work anyway

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -11,13 +11,4 @@ return {
     end
   },
 
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-    config = function()
-      vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>")
-    end
-  },
 }
