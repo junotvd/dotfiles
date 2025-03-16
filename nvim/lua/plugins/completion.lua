95b44e1bf1353bcfd078a2dbba3d72909ce7428d
junot <vdjunot@gmail.com>
2025-03-16 10:35:18 +0100

    lua port of copilot.vim

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,4 +1,5 @@
 return {
+
   {
     'saghen/blink.cmp',
     version = '*',
@@ -31,7 +32,33 @@ return {
       signature = { enabled = true }
     },
   },
+
+  -- {
+  --   "github/copilot.vim",
+  -- },
+
   {
-    "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          hide_during_completion = false,
+          debounce = 25,
+          keymap = {
+            accept = false,
+            accept_word = false,
+            accept_line = "<Tab>",
+            next = false,
+            prev = false,
+            dismiss = false,
+          },
+        },
+      })
+    end,
   },
+
 }
