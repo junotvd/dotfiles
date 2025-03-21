e35b8c1dd40cd4ec6f806ad9f43082a315b6ab80
junot <vdjunot@gmail.com>
2025-03-21 09:23:02 +0100

    show hidden files in oil

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -36,7 +36,13 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    config = function()
+      require("oil").setup({
+        view_options = {
+          show_hidden = true,
+        },
+      })
+    end,
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     lazy = false,
   },
