036782d2b52af31dfaf9821574b6961e6f0f5e2c
junot <vdjunot@gmail.com>
2025-04-08 15:20:55 +0200

    planning option

--- scripts/course-notes.sh
+++ scripts/course-notes.sh
@@ -2,7 +2,7 @@
 
 
 SEMESTER=$(echo -e "2\n1" | rofi -dmenu -i -p "semester" -theme $HOME/dotfiles/rofi/black-white-selector.rasi)
-OPTIE=$(echo -e "hoorcolleges\noefenzittingen" | rofi -dmenu -i -p "hoorcolleges of oefenzittingen" -theme $HOME/dotfiles/rofi/black-white-selector.rasi)
+OPTIE=$(echo -e "hoorcolleges\noefenzittingen\nplanning" | rofi -dmenu -i -p "hoorcolleges of oefenzittingen" -theme $HOME/dotfiles/rofi/black-white-selector.rasi)
 
 if [ -z "$SEMESTER" ] || [ -z "$OPTIE" ]; then
     exit 0
@@ -18,6 +18,19 @@ case "$SEMESTER" in
 esac
 
 BASE_DIR=$(eval echo "$BASE_DIR")
+
+if [ "$OPTIE" == "planning" ]; then
+    PDF_FILE=$(find "$BASE_DIR" -type f \(-name "planning.pdf"\) -print -quit)
+fi
+
+if [ -n "$PDF_FILE" ]; then
+    zathura "$PDF_FILE"
+    exit 1
+else
+    rofi -e "No planning.pdf found in $BASE_DIR/"
+    exit 1
+fi
+
 DIRS=($(find "$BASE_DIR" -maxdepth 1 -type d -not -path "$BASE_DIR" -exec basename {} \;))
 
 if [ ${#DIRS[@]} -eq 0 ]; then
@@ -36,7 +49,7 @@ fi
 
 CHOSEN_PATH="$BASE_DIR/$CHOSEN_DIR/$OPTIE"
 
-PDF_FILE=$(find "$CHOSEN_PATH" -type f \( -name "main.pdf" -o -name "master.pdf" \) -print -quit)
+PDF_FILE=$(find "$CHOSEN_PATH" -type f \( -name "main.pdf" -o -name "master.pdf" -o -name "planning.pdf" \) -print -quit)
 
 if [ -n "$PDF_FILE" ]; then
     zathura "$PDF_FILE"
