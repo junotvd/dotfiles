c5a7c0b7efcd7dda987ff1bd13035444e8da98f6
junot <vdjunot@gmail.com>
2025-04-09 15:23:25 +0200

    add navi plugins

---
+++ nvim-write/lua/plugins/navigation.lua
@@ -0,0 +1,50 @@
+return {
+
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup()
+
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+    end
+  },
+
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
+    end,
+  },
+
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    config = function()
+      require("oil").setup({
+        view_options = {
+          show_hidden = true,
+        },
+      })
+    end,
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    lazy = false,
+  },
+
+}
