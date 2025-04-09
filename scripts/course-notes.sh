a508ccfddf415049e2deeb3477acbf63dd7c3af4
junot <vdjunot@gmail.com>
2025-04-09 15:16:02 +0200

    fixes

--- scripts/course-notes.sh
+++ scripts/course-notes.sh
@@ -2,7 +2,7 @@
 
 
 SEMESTER=$(echo -e "2\n1" | rofi -dmenu -i -p "semester" -theme $HOME/dotfiles/rofi/black-white-selector.rasi)
-OPTIE=$(echo -e "hoorcolleges\noefenzittingen\nplanning" | rofi -dmenu -i -p "hoorcolleges of oefenzittingen" -theme $HOME/dotfiles/rofi/black-white-selector.rasi)
+OPTIE=$(echo -e "hoorcolleges\noefenzittingen\nplanning\nsamenvatting" | rofi -dmenu -i -p "hoorcolleges of oefenzittingen" -theme $HOME/dotfiles/rofi/black-white-selector.rasi)
 
 if [ -z "$SEMESTER" ] || [ -z "$OPTIE" ]; then
     exit 0
@@ -21,15 +21,15 @@ BASE_DIR=$(eval echo "$BASE_DIR")
 
 if [ "$OPTIE" == "planning" ]; then
     PDF_FILE=$(find "$BASE_DIR" -type f -name "planning.pdf" -print -quit)
+    if [ -n "$PDF_FILE" ]; then
+        zathura "$PDF_FILE"
+        exit 1
+        # else
+        #     rofi -e "No planning.pdf found in $BASE_DIR/"
+        #     exit 1
+    fi
 fi
 
-if [ -n "$PDF_FILE" ]; then
-    zathura "$PDF_FILE"
-    exit 1
-else
-    rofi -e "No planning.pdf found in $BASE_DIR/"
-    exit 1
-fi
 
 DIRS=($(find "$BASE_DIR" -maxdepth 1 -type d -not -path "$BASE_DIR" -exec basename {} \;))
 
