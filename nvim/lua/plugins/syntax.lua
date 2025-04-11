6773bf13b6ac527a4d9130db7feae2e416a970aa
junot <vdjunot@gmail.com>
2025-04-11 14:23:01 +0200

    treesitter parsers added

--- nvim/lua/plugins/syntax.lua
+++ nvim/lua/plugins/syntax.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
-        "bash", "c", "lua", "markdown", "matlab", "python",
+        "bash", "c", "lua", "markdown", "matlab", "python", "typst", "vimdoc",
       },
       sync_install = false,
       auto_install = true,
