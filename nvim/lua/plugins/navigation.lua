579b457bafa09d9e8530aa177930e3fd707ccc3e
junot <vdjunot@gmail.com>
2025-04-12 16:09:46 +0200

    telescope keybinds

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -18,23 +18,36 @@ return {
   },
 
   {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
+      -- from kickstart.nvim
+      vim.keymap.set("n", "<leader>/", function()
+        builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+          previewer = false,
+        })
+      end)
+        vim.keymap.set('n', '<leader>s/', function()
+          builtin.live_grep {
+            grep_open_files = true,
+            prompt_title = 'Live Grep in Open Files',
+          }
+      end)
+      pcall(require('telescope').load_extension, 'fzf')
     end,
   },
 
   {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
+    "stevearc/oil.nvim",
+    ---@module "oil"
     ---@type oil.SetupOpts
     config = function()
       require("oil").setup({
