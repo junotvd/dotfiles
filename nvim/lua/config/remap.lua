4538aceacfb222f3b677b24013ece60258e51d48
junot <vdjunot@gmail.com>
2025-04-12 16:09:30 +0200

    hl settings

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pv", vim.cmd.Oil)
 
