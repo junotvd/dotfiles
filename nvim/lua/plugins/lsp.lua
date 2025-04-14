dbc83397d0f63c7788898d468d6837deefbd8ef4
junot <vdjunot@gmail.com>
2025-04-14 11:16:07 +0200

    expanding config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,40 +1,90 @@
 return {
 
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "saghen/blink.cmp",
-    {
-      "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-        library = {
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+
+      { "j-hui/fidget.nvim", opts = {} },
+
+      {
+        "saghen/blink.cmp",
+        version = "*",
+        dependencies = {
+          -- 'rafamadriz/friendly-snippets',
+          {
+            "L3MON4D3/LuaSnip",
+            version = "v2.*",
+            config = function()
+              require("luasnip").config.set_config({
+                history = true,
+                enable_autosnippets = true,
+                require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" }),
+              })
+            end,
+          },
+        },
+        opts = {
+          sources = {
+            default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+            providers = {
+              lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+            },
+          },
+          snippets = { preset = "luasnip" },
+          keymap = { preset = "default" },
+          appearance = {
+            use_nvim_cmp_as_default = true,
+            nerd_font_variant = "mono",
+          },
+          signature = { enabled = true },
+        },
+      },
+
+    },
+    opts = {
+      servers = {
+        lua_ls = {},
+        texlab = {},
+        pyright = {},
+        bashls = {},
+        tinymist = {
+          settings = {
+            formatterMode = "typstyle",
+          },
         },
       },
     },
-  },
-  opts = {
-    servers = {
-      lua_ls = {},
-      texlab = {},
-      pyright = {},
-      bashls = {},
-      tinymist = {
-        settings = {
-          formatterMode = "typstyle"
-        }
-      },
-    },
+
+    config = function(_, opts)
+      local lspconfig = require("lspconfig")
+      for server, config in pairs(opts.servers) do
+        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+    end,
+    -- vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
   },
 
-  config = function(_, opts)
-    local lspconfig = require("lspconfig")
-    for server, config in pairs(opts.servers) do
-      config.capabilities =
-      require("blink.cmp").get_lsp_capabilities(config.capabilities)
-      lspconfig[server].setup(config)
-    end
-  end
-    -- vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
+  {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+        },
+      })
+    end,
+  },
 
 }
--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -1,63 +1,81 @@
 return {
 
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
 
-    config = function()
-      local harpoon = require("harpoon")
-      harpoon:setup()
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup()
 
-      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():add()
+			end)
+			vim.keymap.set("n", "<C-e>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
 
-      vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
-      vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
-    end
-  },
+			vim.keymap.set("n", "<C-p>", function()
+				harpoon:list():prev()
+			end)
+			vim.keymap.set("n", "<C-n>", function()
+				harpoon:list():next()
+			end)
+		end,
+	},
 
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
-      vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
-      -- from kickstart.nvim
-      vim.keymap.set("n", "<leader>/", function()
-        builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
-          previewer = false,
-        })
-      end)
-        vim.keymap.set('n', '<leader>s/', function()
-          builtin.live_grep {
-            grep_open_files = true,
-            prompt_title = 'Live Grep in Open Files',
-          }
-      end)
-      pcall(require('telescope').load_extension, 'fzf')
-    end,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			{ "nvim-tree/nvim-web-devicons", enabled = true },
+		},
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(opts),
+					},
+				},
+			})
 
-  {
-    "stevearc/oil.nvim",
-    ---@module "oil"
-    ---@type oil.SetupOpts
-    config = function()
-      require("oil").setup({
-        view_options = {
-          show_hidden = true,
-        },
-      })
-    end,
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    lazy = false,
-  },
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
 
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
+			vim.keymap.set("n", "<leader>/", function()
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					previewer = false,
+				}))
+			end)
+			vim.keymap.set("n", "<leader>s/", function()
+				builtin.live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end)
+		end,
+	},
+
+	{
+		"stevearc/oil.nvim",
+		---@module "oil"
+		---@type oil.SetupOpts
+		config = function()
+			require("oil").setup({
+				view_options = {
+					show_hidden = true,
+				},
+			})
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
+		lazy = false,
+	},
 }
