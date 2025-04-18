f15f0dfbacac7187797c8c74f8e2a13d5641d8a7
junot <vdjunot@gmail.com>
2025-04-18 11:50:45 +0200

    niet nodig hier

--- nvim-exp/lua/snippets/tex/utils/init.lua
+++
@@ -1,7 +0,0 @@
--- https://github.com/evesdropper/dotfiles/blob/main/nvim/lua/snippets/tex/utils/init.lua
-local M = {}
-
-M.conditions = require("snippets.tex.utils.conditions")
-M.scaffolding = require("snippets.tex.utils.scaffolding")
-
-return M
