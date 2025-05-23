80c9e541c671561a208bcb0b12c7e6d6e0d5d1f7
junot <vdjunot@gmail.com>
2025-05-23 15:10:28 +0200

    chatgpt ingeschakeld

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,46 +1,40 @@
+import System.IO (hPutStrLn)
 import XMonad
-import XMonad.Hooks.EwhmDesktops
+import XMonad.Config.Desktop
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Layout.BinarySpacePartition
-import XMonad.Util.EZConfig
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.Run (spawnPipe)
 
-main :: IO ()
-main = xmonad $ ewmh $ docks myConfig
-
-myConfig = def
-    { modMask = mod1Mask
-    , layoutHook = myLayout
-    , manageHook = myManageHook
-    , startupHook =
-    , myStartupHook
-}
-`additionalKeysP` myKeys
-
-
-myTerminal :: String
-myTerminal = "wezterm"
-
-myBrowser :: String
-myBrowser = "firefox"
-
-myKeys :: [(Char, X ())]
-myKeys =
-[ ("M-t", spawn myTerminal),
-  ("M-b", spawn myBrowser)
-]
-
-myManageHook :: ManageHook
-myManageHook =
-composeAll
-[ className =? "Gimp" --> doFloat,
-    isDialog --> doFloat
-]
-
-myLayout = tiled ||| Mirror tiled ||| full ||| threeCol
-    where
-    tiled = Tall 1 (3 / 100) (1 / 2)
-threeCol = ThreeColMid 1 (3 / 100) (1 / 2)
-
-    myStartupHook = do
-    spawm "nm-applet"
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad $
+    docks
+      desktopConfig
+        { modMask = mod1Mask -- Use Super instead of Alt
+        , terminal = "wezterm"
+        , borderWidth = 2
+        , normalBorderColor = "#282c34"
+        , focusedBorderColor = "#61afef"
+        , layoutHook = avoidStruts $ layoutHook desktopConfig
+        , manageHook = manageDocks <+> manageHook desktopConfig
+        , logHook =
+            dynamicLogWithPP
+              xmobarPP
+                { ppOutput = hPutStrLn xmproc
+                , ppTitle = xmobarColor "#98c379" "" . shorten 60
+                }
+        }
+      `additionalKeysP` [ ("M-t", spawn "wezterm")
+                        , ("M-d", spawn "dmenu_run")
+                        , ("M-S-q", kill)
+                        , ("M-<Left>", windows W.focusLeft)
+                        , ("M-<Right>", windows W.focusRight)
+                        , ("M-<Up>", windows W.focusUp)
+                        , ("M-<Down>", windows W.focusDown)
+                        , ("M-S-<Left>", windows W.swapLeft)
+                        , ("M-S-<Right>", windows W.swapRight)
+                        , ("M-S-<Up>", windows W.swapUp)
+                        , ("M-S-<Down>", windows W.swapDown)
+                        ]
