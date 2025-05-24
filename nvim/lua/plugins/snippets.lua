0c429430407198257d490fcc36364102675e32f0
junot <vdjunot@gmail.com>
2025-05-24 21:53:01 +0200

    luasnip settings

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -4,25 +4,45 @@ return {
     'L3MON4D3/LuaSnip',
     version = 'v2.*',
     build = 'make install_jsregexp',
-    dependencies = {},
     config = function()
-      require('luasnip').config.set_config({
+      require('luasnip.loaders.from_lua').lazy_load({
+        paths = '~/.config/nvim/lua/luasnippets/snippets/',
+      })
+
+      local ls = require('luasnip')
+      vim.keymap.set({ 'i' }, '<Tab>', function()
+        ls.expand()
+      end, { silent = true })
+      vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+        ls.jump(1)
+      end, { silent = true })
+      vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
+        ls.jump(-1)
+      end, { silent = true })
+
+      vim.keymap.set({ 'i', 's' }, '<C-E>', function()
+        if ls.choice_active() then
+          ls.change_choice(1)
+        end
+      end, { silent = true })
+
+      ls.config.set_config({
         history = true,
         enable_autosnippets = true,
-        require('luasnip.loaders.from_lua').load({ paths = './lua/luasnips' }),
+        store_selection_keys = '<Tab>',
       })
     end,
   },
 
-  {
-    dir = '~/repos/luasnip-latex-snippets.nvim',
-    -- vimtex isn't required if using treesitter
-    dependencies = { 'L3MON4D3/LuaSnip', 'lervag/vimtex' },
-    config = function()
-      require('luasnip-latex-snippets').setup({
-        -- or setup({ use_treesitter = true })
-        allow_on_markdown = true,
-      })
-    end,
-  },
+  -- {
+  --   dir = '~/repos/luasnip-latex-snippets.nvim',
+  --   -- vimtex isn't required if using treesitter
+  --   dependencies = { 'L3MON4D3/LuaSnip', 'lervag/vimtex' },
+  --   config = function()
+  --     require('luasnip-latex-snippets').setup({
+  --       -- or setup({ use_treesitter = true })
+  --       allow_on_markdown = true,
+  --     })
+  --   end,
+  -- },
 }
