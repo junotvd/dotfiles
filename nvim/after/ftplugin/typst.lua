6dcc27b670919a07fce19d39d602cb744070e5ff
junot <vdjunot@gmail.com>
2025-06-09 15:33:31 +0200

    iets geprobeerd

--- nvim/after/ftplugin/typst.lua
+++ nvim/after/ftplugin/typst.lua
@@ -1,3 +1,4 @@
 local typst_watch = require('typst').watch
 vim.keymap.set('n', '<leader>ll', typst_watch)
+-- vim.keymap.set('n', '<leader>lc', ':!typst compile --font-path ~/fonts/cmbright/fonts/ %<CR>')
 vim.keymap.set('n', '<leader>lc', ':!typst compile %<CR>')
