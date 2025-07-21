9148b0f59a602bda3fcb787c1fb5f2f6420ed9ab
junot <vdjunot@gmail.com>
2025-07-21 15:40:28 +0200

    idk

--- nvim/after/ftplugin/typst.lua
+++ nvim/after/ftplugin/typst.lua
@@ -2,3 +2,6 @@ local typst_watch = require('typst').watch
 vim.keymap.set('n', '<leader>ll', typst_watch)
 -- vim.keymap.set('n', '<leader>lc', ':!typst compile --font-path ~/fonts/cmbright/fonts/ %<CR>')
 vim.keymap.set('n', '<leader>lc', ':!typst compile %<CR>')
+
+-- local export_picker = require('export').export_picker
+-- vim.keymap.set('n', '<leader>le', export_picker)
