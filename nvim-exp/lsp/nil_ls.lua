41b2ef5fc4a34bf479c3c6da9231aef7ac704746
junot <vdjunot@gmail.com>
2025-12-28 02:42:03 +0100

    nix formatting

--- nvim-exp/lsp/nil_ls.lua
+++ nvim-exp/lsp/nil_ls.lua
@@ -11,7 +11,14 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'nil' },
-  filetypes = { 'nix' },
-  root_markers = { 'flake.nix', '.git' },
+	cmd = { 'nil' },
+	filetypes = { 'nix' },
+	root_markers = { 'flake.nix', '.git' },
+	settings = {
+		['nil'] = {
+			formatting = {
+				command = { 'alejandra' }
+			},
+		},
+	},
 }
