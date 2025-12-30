e6e8493293711ad55c9d665109cbd3f023138149
junot <vdjunot@gmail.com>
2025-12-30 21:30:35 +0100

    bubuh

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 local opt = vim.opt
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 opt.number = true
 opt.relativenumber = true
 opt.wrap = false
@@ -9,73 +9,72 @@ opt.smartindent = true
 opt.cursorcolumn = false
 opt.ignorecase = true
 opt.smartcase = true
-opt.signcolumn = 'yes'
+opt.signcolumn = "yes"
 opt.swapfile = false
 opt.termguicolors = true
 opt.undofile = true
 opt.incsearch = true
 opt.hlsearch = false
-opt.guicursor = ''
-opt.mouse = ''
+opt.guicursor = ""
+opt.mouse = ""
 opt.scrolloff = 100
 opt.spell = true
-opt.spelllang = { 'nl', 'en_gb' }
+opt.spelllang = { "nl", "en_gb" }
 
-
-vim.pack.add {
-	{ src = 'https://github.com/rose-pine/neovim',                name = 'rosepine' },
-	{ src = 'https://github.com/vague2k/vague.nvim' },
-	{ src = 'https://github.com/y9san9/y9nika.nvim' },
-	{ src = 'https://github.com/ricardoraposo/gruvbox-minor.nvim' },
-	{ src = 'https://github.com/savq/melange-nvim' },
-	{ src = 'https://github.com/RostislavArts/naysayer.nvim' },
-	{ src = 'https://github.com/stevearc/oil.nvim' },
-	{ src = 'https://github.com/echasnovski/mini.pick' },
-	{ src = 'https://github.com/echasnovski/mini.surround' },
-	{ src = 'https://github.com/chomosuke/typst-preview.nvim' },
-	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+vim.pack.add({
+	{ src = "https://github.com/rose-pine/neovim", name = "rosepine" },
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/y9san9/y9nika.nvim" },
+	{ src = "https://github.com/ricardoraposo/gruvbox-minor.nvim" },
+	{ src = "https://github.com/savq/melange-nvim" },
+	{ src = "https://github.com/RostislavArts/naysayer.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/echasnovski/mini.surround" },
+	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{
-		src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
 		data = {
 			run = function(_)
-				vim.cmd 'TSUpdate'
+				vim.cmd("TSUpdate")
 			end,
 		},
 	},
-	{ src = 'https://github.com/L3MON4D3/LuaSnip' },
-	{ src = 'https://github.com/mason-org/mason.nvim' },
-	{ src = 'https://github.com/stevearc/conform.nvim' },
-	{ src = 'https://github.com/chentoast/marks.nvim' },
-}
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/stevearc/conform.nvim" },
+	{ src = "https://github.com/chentoast/marks.nvim" },
+})
 
 vim.lsp.enable({
-	'lua_ls',
-	'tinymist',
-	'basedpyright',
-	'ruff',
-	'html',
-	'cssls',
-	'bashls',
-	'nil',
-	'vtsls',
-	'gopls',
-	'rust_analyzer',
+	"lua_ls",
+	"tinymist",
+	"basedpyright",
+	"ruff",
+	"html",
+	"cssls",
+	"bashls",
+	"nil",
+	"vtsls",
+	"gopls",
+	"rust_analyzer",
 })
 
-vim.diagnostic.config {
+vim.diagnostic.config({
 	virtual_text = { severity = vim.diagnostic.severity.ERROR },
-	severity_sort = true
-}
-
-require('conform').setup({
-	lsp_format = 'never',
-	formatters_by_ft = {
-		sh = { 'shfmt' },
-	}
+	severity_sort = true,
 })
 
-require 'marks'.setup { builtin_marks = { '<', '>', '^', '"', [[']], '`', '[', ']' } }
-require('mason').setup()
+require("conform").setup({
+	lsp_format = "never",
+	formatters_by_ft = {
+		sh = { "shfmt" },
+	},
+})
+
+require("marks").setup({ builtin_marks = { "<", ">", "^", '"', [[']], "`", "[", "]" } })
+require("mason").setup()
 
 -- vim.api.nvim_create_user_command('Mason', function()
 -- 	pcall(vim.cmd.packadd, 'mason.nvim')
@@ -83,79 +82,83 @@ require('mason').setup()
 -- 	vim.cmd('Mason')
 -- end, {})
 
-require 'mini.pick'.setup({
+require("mini.pick").setup({
 	window = {
 		config = function()
 			local height = math.floor(0.618 * vim.o.lines)
 			local width = math.floor(0.618 * vim.o.columns)
 			return {
-				anchor = 'NW',
+				anchor = "NW",
 				height = height,
 				width = width,
 				row = math.floor(0.5 * (vim.o.lines - height)),
 				col = math.floor(0.5 * (vim.o.columns - width)),
 			}
-		end
-	}
+		end,
+	},
 })
-require 'mini.surround'.setup()
-require 'oil'.setup {
+require("mini.surround").setup()
+require("oil").setup({
 	default_file_explorer = true,
 	view_options = {
 		show_hidden = true,
 	},
 	columns = {
-		'permissions',
-		'size',
+		"permissions",
+		"size",
 	},
-}
-
-vim.api.nvim_create_autocmd('InsertEnter', {
-	once = true,
-	callback = function()
-		pcall(vim.cmd.packadd, 'LuaSnip')
-		local ls = require('luasnip')
-		ls.setup { enable_autosnippets = true }
-		require('luasnip.loaders.from_lua').lazy_load({
-			paths = { vim.fn.expand('~/dotfiles/nvim/snippets/') },
-		})
-		local map = vim.keymap.set
-		map('i', '<C-e>', function() ls.expand() end, { silent = true })
-		map({ 'i', 's' }, '<C-J>', function() ls.jump(1) end, { silent = true })
-		map({ 'i', 's' }, '<C-K>', function() ls.jump(-1) end, { silent = true })
-	end
 })
 
-require 'rose-pine'.setup { styles = {
-	transparency = true
-}}
-vim.cmd.colorscheme 'rose-pine'
-vim.cmd ':hi statusline guibg=NONE'
+vim.api.nvim_create_autocmd("InsertEnter", {
+	once = true,
+	callback = function()
+		pcall(vim.cmd.packadd, "LuaSnip")
+		local ls = require("luasnip")
+		ls.setup({ enable_autosnippets = true })
+		require("luasnip.loaders.from_lua").lazy_load({
+			paths = { vim.fn.expand("~/dotfiles/nvim/snippets/") },
+		})
+		local map = vim.keymap.set
+		map("i", "<C-e>", function()
+			ls.expand()
+		end, { silent = true })
+		map({ "i", "s" }, "<C-J>", function()
+			ls.jump(1)
+		end, { silent = true })
+		map({ "i", "s" }, "<C-K>", function()
+			ls.jump(-1)
+		end, { silent = true })
+	end,
+})
 
-vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('my.lsp', {}),
+require("rose-pine").setup({ styles = {
+	transparency = true,
+} })
+require("vague").setup({ transparent = true })
+vim.cmd.colorscheme("vague")
+vim.cmd(":hi statusline guibg=NONE")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("my.lsp", {}),
 	callback = function(args)
 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		if client:supports_method 'textDocument/completion' then
+		if client:supports_method("textDocument/completion") then
 			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
 			local chars = {}
 			for i = 32, 126 do
 				table.insert(chars, string.char(i))
 			end
 			client.server_capabilities.completionProvider.triggerCharacters = chars
-			vim.lsp.completion.enable(
-				true,
-				client.id,
-				args.buf,
-				{ autotrigger = true }
-			)
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
 		end
 	end,
 })
-vim.opt.completeopt:append({ 'menuone', 'noselect', 'popup' })
+vim.opt.completeopt:append({ "menuone", "noselect", "popup" })
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-	callback = function() vim.highlight.on_yank() end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 local function pack_clean()
@@ -173,49 +176,48 @@ local function pack_clean()
 	end
 
 	if #unused_plugins == 0 then
-		print 'No unused plugins.'
+		print("No unused plugins.")
 		return
 	end
 
-	local choice = vim.fn.confirm('Remove unused plugins?', '&Yes\n&No', 2)
+	local choice = vim.fn.confirm("Remove unused plugins?", "&Yes\n&No", 2)
 	if choice == 1 then
 		vim.pack.del(unused_plugins)
 	end
 end
 
-
 local map = vim.keymap.set
 
-map('n', '<leader>o', '<Cmd>update<CR> <Cmd>source<CR>')
-map('n', '<leader>O', '<Cmd>source $MYVIMRC<CR>')
+map("n", "<leader>o", "<Cmd>update<CR> <Cmd>source<CR>")
+map("n", "<leader>O", "<Cmd>source $MYVIMRC<CR>")
 
-map({ 'n', 'v', 'x' }, '<leader>y', '"+y')
-map({ 'n', 'v', 'x' }, '<leader>d', '"+d')
+map({ "n", "v", "x" }, "<leader>y", '"+y')
+map({ "n", "v", "x" }, "<leader>d", '"+d')
 
-map('v', 'J', ":m '>+1<CR>gv=gv")
-map('v', 'K', ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
-map('n', '<leader>pv', vim.cmd.Oil)
-map('n', '<leader>f', ':Pick files<CR>')
-map('n', '<leader>h', ':Pick help<CR>')
-map('n', '<leader>b', ':Pick buffers<CR>')
-map('n', '<leader>g', ':Pick grep<CR>')
+map("n", "<leader>pv", vim.cmd.Oil)
+map("n", "<leader>f", ":Pick files<CR>")
+map("n", "<leader>h", ":Pick help<CR>")
+map("n", "<leader>b", ":Pick buffers<CR>")
+map("n", "<leader>g", ":Pick grep<CR>")
 
-map('n', '<leader>lf', vim.lsp.buf.format)
-map('n', '<leader>lF', require('conform').format)
+map("n", "<leader>lf", vim.lsp.buf.format)
+map("n", "<leader>lF", require("conform").format)
 
-map({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')
-map({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
-map({ 'n', 'v', 'x' }, '<leader>v', ':e $MYVIMRC<CR>')
-map('n', '<leader>r', ':update<CR> :make<CR>')
+map({ "n", "v", "x" }, "<leader>s", ":e #<CR>")
+map({ "n", "v", "x" }, "<leader>S", ":sf #<CR>")
+map({ "n", "v", "x" }, "<leader>v", ":e $MYVIMRC<CR>")
+map("n", "<leader>r", ":update<CR> :make<CR>")
 
-map('n', '<C-q>', ':copen<CR>', { silent = true })
+map("n", "<C-q>", ":copen<CR>", { silent = true })
 
-map('n', '<leader>t', '<Cmd>tabnew<CR>')
-map('n', '<leader>x', '<Cmd>tabclose<CR>')
+map("n", "<leader>t", "<Cmd>tabnew<CR>")
+map("n", "<leader>x", "<Cmd>tabclose<CR>")
 for i = 1, 9 do
-	map('n', '<leader>' .. i, '<Cmd>tabnext ' .. i .. '<CR>', { noremap = true, silent = true })
+	map("n", "<leader>" .. i, "<Cmd>tabnext " .. i .. "<CR>", { noremap = true, silent = true })
 end
 
-map('n', '<leader>pc', pack_clean)
-map('n', '<leader>pu', '<cmd>lua vim.pack.update()<CR>')
+map("n", "<leader>pc", pack_clean)
+map("n", "<leader>pu", "<cmd>lua vim.pack.update()<CR>")
