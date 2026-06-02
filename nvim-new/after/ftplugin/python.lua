e62eae2ad99dade1b1731561f31e0fd4f820f5aa
junot <vdjunot@gmail.com>
2026-06-02 09:15:24 +0200

    nvim | handig ftplugin

---
+++ nvim-new/after/ftplugin/python.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<leader>r', '<Cmd>split | terminal python3 %<CR>', { buffer = true })
+vim.keymap.set('n', '<leader>t', '<Cmd>split | terminal pytest %<CR>', { buffer = true })
--- nvim-new/after/ftplugin/typst.lua
+++ nvim-new/after/ftplugin/typst.lua
@@ -1,7 +1,24 @@
 vim.cmd('setlocal spell')
 
-vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>c', '<Cmd>!typst c %<CR>', { silent = true })
-vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>o', '<Cmd>!typst c --open zathura %<CR>', { silent = true })
+local map = vim.keymap.set
+map('n', '<Leader>c', '<Cmd>!typst c %<CR>', { buffer = true, silent = true })
+map('n', '<Leader>O', '<Cmd>!typst c --open zathura %<CR>', { buffer = true, silent = true })
+map('n', '<leader>o', function()
+  local pdf = vim.fn.expand('%:r') .. '.pdf'
+  vim.fn.jobstart({ 'xdg-open', pdf }, { detach = true })
+end, { buffer = true })
+local watch_job = nil
+map('n', '<leader>w', function()
+  if watch_job then
+    vim.fn.jobstop(watch_job)
+    watch_job = nil
+    vim.notify('Stopped typst watch')
+  else
+    watch_job = vim.fn.jobstart({ 'typst', 'watch', vim.fn.expand('%s') })
+    vim.notify('Started typst watch')
+  end
+end, { buffer = true })
+map('n', '<leader>v', '<Cmd>TypstPreviewToggle<CR>', { buffer = true, silent = true })
 
 -- bekijk dit eens https://github.com/SylvanFranklin/.config/blob/main/nvim/after/ftplugin/typst.lua;
 -- ziet er wel cool uit met die TS text objects, misschien handig, alsook snippets terugvinden.
